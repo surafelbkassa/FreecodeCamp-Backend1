17dd29b3715278be4a3b34941123a89a423922de
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2024-12-05 07:46:21 +0300

    basic functionality
    basic functionality

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,20 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 const app=express();
 const port=3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
     res.render("index.ejs")
 })
+// app.get('/',(req,res)=>{
+//     res.render("index.ejs")
+// })
+app.post('/submit',(req,res)=>{
+    res.render("index.ejs",{Number1:req.body["ToDONumber1"],Number2:req.body["ToDoNumber2"],text:req.body["Textarea"]})
+})
+
 app.listen(port,()=>{
     console.log(`Litsening at port ${port}`);
     
