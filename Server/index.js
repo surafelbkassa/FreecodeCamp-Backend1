524e6f20d62cabf09b2ce3e4b9be9de730e897fb
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2024-12-05 04:50:57 +0300

    add inputs in ejs

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app=express();
 const port=3000;
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.render("index.ejs")
+})
 app.listen(port,()=>{
     console.log(`Litsening at port ${port}`);
     
